8fc2244bc8de2c5f23d8020ba118fa99e2197321
Christopher Powroznik <christopher@expopulus.com>
2024-06-10 21:07:27 -0700

    feat: add grid of images

---
+++ entries.js
@@ -0,0 +1,23 @@
+const entries = [
+    {
+        timestamp: new Date('October 9, 2023'),
+    },
+    {
+        timestamp: new Date('December 10, 2023'),
+    },
+    {
+        timestamp: new Date('January 9, 2024'),
+    },
+    {
+        timestamp: new Date('January 29, 2024'),
+    },
+    {
+        timestamp: new Date('February 20, 2024'),
+    },
+    {
+        timestamp: new Date('April 3, 2024'),
+    },
+    {
+        timestamp: new Date('May 26, 2024'),
+    }
+]
