c56744e584fd1429c87f68a5e75cc209261dcd30
dev <developer@developer.com>
2024-08-28 00:21:10 -0700

    Change layout

--- entries.js
+++ entries.js
@@ -1,32 +1,42 @@
 const entries = [
     {
         timestamp: new Date('October 9, 2023'),
+        image: null // No image for the first entry
     },
     {
         timestamp: new Date('December 10, 2023'),
+        image: '2.jpeg'
     },
     {
         timestamp: new Date('January 9, 2024'),
+        image: '3.jpeg'
     },
     {
         timestamp: new Date('January 29, 2024'),
+        image: '4.jpeg'
     },
     {
         timestamp: new Date('February 20, 2024'),
+        image: '5.jpeg'
     },
     {
         timestamp: new Date('April 3, 2024'),
+        image: '6.jpeg'
     },
     {
         timestamp: new Date('May 26, 2024'),
+        image: '7.jpeg'
     },
     {
         timestamp: new Date('July 29, 2024'),
+        image: '8.jpeg'
     },
     {
         timestamp: new Date('August 15, 2024'),
+        image: '9.jpeg'
     },
     {
         timestamp: new Date('August 23, 2024'),
+        image: '10.jpeg'
     },
 ]
